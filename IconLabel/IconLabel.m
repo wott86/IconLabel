0c8183a804640d9d0e8bd9937dd7c451d8596e0a
César Vilera <cesarvilera8602@gmail.com>
2015-10-16 17:32:22 -0430

    Icon resized to match the UILabel height

--- IconLabel/IconLabel.m
+++ IconLabel/IconLabel.m
@@ -10,7 +10,6 @@
 
 @interface IconLabel ()
 @property (strong, nonatomic) UIImage *icon;
-@property CGRect iconFrame;
 
 @end
 
@@ -19,18 +18,36 @@
 -(id)initWithFrame:(CGRect)frame text:(NSString *)text andIcon:(UIImage *)icon {
     self = [super initWithFrame:frame];
     if (self) {
-        [self setText:text andIcon:icon];
+        self.icon = icon;
+        [self setText:text andIcon:self.icon];
+    }
+    return self;
+}
+
+-(id)initWithFrame:(CGRect)frame {
+    self = [super initWithFrame:frame];
+    if (self) {
+        
+    }
+    return self;
+}
+
+-(id)initWithCoder:(NSCoder *)aDecoder {
+    self = [super initWithCoder:aDecoder];
+    if (self) {
     }
     return self;
 }
 
 -(void)setText:(NSString *)text andIcon:(UIImage *)icon {
-    _icon = icon;
     NSMutableAttributedString *textAttrString= [[NSMutableAttributedString alloc] init];
     if (icon) {
         NSTextAttachment *attachment = [[NSTextAttachment alloc] init];
         attachment.image = icon;
-        attachment.bounds = (_iconFrame.size.height && _iconFrame.size.width) ? _iconFrame : CGRectMake(0, self.font.descender/2, MAX(icon.size.width, icon.size.height), MAX(icon.size.width, icon.size.height));
+        float bottom = self.font.descender;
+        float size = self.font.lineHeight;
+        float dif = MAX(icon.size.height, size) - MIN(icon.size.height, size);
+        attachment.bounds = CGRectMake(0, bottom + (dif/2), icon.size.width, icon.size.height);
         NSAttributedString *iconAttrString = [NSAttributedString attributedStringWithAttachment:attachment];
         [textAttrString appendAttributedString:iconAttrString];
     }
@@ -47,13 +64,24 @@
     [self setText:text andIcon:_icon];
 }
 
--(void)setIcon:(UIImage *)icon {
-    [self setText:self.text andIcon:_icon];
-}
-
 -(UIImage *)getIcon {
     return self.icon;
 }
 
+-(void)setIcon:(UIImage *)icon {
+    if (icon) {
+        if (self.frame.size.height > 0 && icon.size.height > self.frame.size.height) {
+            CGSize size = CGSizeMake(icon.size.width, self.frame.size.height);
+            UIGraphicsBeginImageContext(size);
+            [icon drawInRect:CGRectMake(0, 0, size.width, size.height)];
+            UIImage *newImage = UIGraphicsGetImageFromCurrentImageContext();
+            UIGraphicsEndImageContext();
+            _icon = newImage;
+        }else {
+            _icon = icon;
+        }
+    }
+    [self setText:self.text andIcon:self.icon];
+}
 
 @end
